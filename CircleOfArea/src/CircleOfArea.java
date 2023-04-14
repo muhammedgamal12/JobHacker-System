9d0b38db6076e481bc9514776a3f0c42171dbc91
muhammedgamal12 <45471321+muhammedgamal12@users.noreply.github.com>
2023-04-14 18:18:44 +0200

    Update CircleOfArea.java

--- CircleOfArea/src/CircleOfArea.java
+++ CircleOfArea/src/CircleOfArea.java
@@ -1,7 +1,10 @@
 public class CircleOfArea {
     public static void main(String[] args)
     {
-        double r = 2;
+    
+    Scanner radius = new Scanner(System.in);
+        double r = radius.nextDouble();
+
         final  double n = 3.14159;
         double Area = n * (r * r);
 
