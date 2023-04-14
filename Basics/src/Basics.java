4ef3ec2b169f7433ec8d93dd61f04923fbea44f0
muhammedgamal12 <45471321+muhammedgamal12@users.noreply.github.com>
2023-04-14 18:20:40 +0200

    Update Basics.java

--- Basics/src/Basics.java
+++ Basics/src/Basics.java
@@ -1,9 +1,13 @@
 public class Basics {
     public static void main(String[] args)
     {
-        int A = 5;
-        int B = -7;
-        int x = A + B;
+        Scanner numbers=new Scanner(System.in);
+        
+        int a = numbers.nextInt();
+        int b = numbers.nextInt();
+
+        int x= a + b;
+        
         System.out.println("x = "+x);
     }
 }
