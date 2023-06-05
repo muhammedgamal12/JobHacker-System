293457baaf110857985b4642a5c4660860448054
muhamed gamal <m.gamal2288@gmail.com>
2023-06-05 20:27:08 +0300

    week3 Tasks

--- Tasks/src/AddingTwoArrays.java
+++ Tasks/src/AddingTwoArrays.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Adding_TwoArrays
 {
+
     public  static void main (String [] args)
     {
         int [] array1 = {2,5,1,8,4,66};
@@ -13,23 +16,22 @@ public class Adding_TwoArrays
 
     }
 
-    public static int [] adding_array(int [] arr1,int []arr2)
+    public static  ArrayList<Integer> adding_array(int [] arr1, int []arr2)
     {
-        int [] result = new int[arr1.length];
+        ArrayList <Integer> result =new ArrayList();
 
             for (int i =0 ;i < arr1.length ; i++)
             {
-                result [i] =arr1[i]+arr2[i];
+                result.add( arr1[i] + arr2[i]);
             }
 
 
         return  result;
     }
     //print array from Integer datatype
-    public static void print_array(int [] the_array) {
-        for (int i =0 ; i< the_array.length ; i++)
-        {
-            System.out.print(the_array[i] +",");
+    public static void print_array(ArrayList <Integer> the_array) {
+        for (int e:the_array) {
+            System.out.print(e + ",");
         }
     }
 }
